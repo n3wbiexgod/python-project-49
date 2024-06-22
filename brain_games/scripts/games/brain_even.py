7464a9e701a2c4a73522237d009e41aa6b8ea1fc
Gridnev Mikhail <greeddyman@gmail.com>
2024-06-22 10:09:53 +0400

    cognitive complexity fix

--- brain_games/scripts/games/brain_even.py
+++ brain_games/scripts/games/brain_even.py
@@ -1,6 +1,16 @@
 from random import randint
 from brain_games.cli import welcome_user
 
+
+def get_user_answer(number):
+    print(f'Question: {number}')
+    return input('Your answer: ')
+
+
+def is_answer_correct(number, user_answer):
+    return 'yes' if number % 2 == 0 else 'no' == user_answer
+
+
 def main():
     name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
@@ -8,14 +18,12 @@ def main():
     correct_answers_count = 0
     while correct_answers_count < 3:
         number = randint(1, 100)
-        print(f'Question: {number}')
-        user_answer = input('Your answer: ')
-        correct_answer = 'yes' if number % 2 == 0 else 'no'
-
-        if user_answer == correct_answer:
+        user_answer = get_user_answer(number)
+        if is_answer_correct(number, user_answer):
             print('Correct!')
             correct_answers_count += 1
         else:
+            correct_answer = 'yes' if number % 2 == 0 else 'no'
             print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
