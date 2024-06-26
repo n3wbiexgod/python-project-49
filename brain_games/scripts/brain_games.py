0ffb472f0751630e377ef95b77592f9010a1c1f9
Gridnev Mikhail <greeddyman@gmail.com>
2024-06-26 10:20:35 +0400

    fixed brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 # brain_games/scripts/brain_games.py
 
+from brain_games.cli import welcome_user
+
+
 def main():
-    print("Welcome to the Brain Games!")
+    welcome_user()
 
 if __name__ == '__main__':
     main()
