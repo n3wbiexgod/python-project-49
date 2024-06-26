52b5566a4e4ee5d35dc3eb5af5035ecd8120f356
Gridnev Mikhail <greeddyman@gmail.com>
2024-06-26 18:02:01 +0400

    linter fixes

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -3,27 +3,39 @@ import prompt
 
 
 def gcd(number1, number2):
+
     while number2:
         number1, number2 = number2, number1 % number2
     return number1
 
 
 def main():
+
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!\nFind the greatest common divisor of given numbers.')
+
     for _ in range(3):
         num1 = random.randint(1, 100)
         num2 = random.randint(1, 100)
-        print(f'Question: {num1} {num2}')
+        question = f'Question: {num1} {num2}'
+        print(question[:79])
+
         answer = prompt.string('Your answer: ')
         correct_answer = gcd(num1, num2)
+
         if int(answer) == correct_answer:
             print('Correct!')
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\nLet's try again, {name}!")
+            error_message = (
+                f"'{answer}' is wrong answer ;(. Correct answer was "
+                f"'{correct_answer}'.\nLet's try again, {name}!"
+            )
+            print(error_message[:79])
             return
+
     print(f'Congratulations, {name}!')
+
 
 if __name__ == '__main__':
     main()
