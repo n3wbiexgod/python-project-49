9029c20fca054a823bc4f72b4afb373897584190
Michael <168862237+n3wbiexgod@users.noreply.github.com>
2024-07-01 15:34:15 +0400

    Update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -11,5 +11,5 @@ OPERATORS = {
 def make_question():
     first_number, second_number = randint(1, 50), randint(1, 50)
     operator = choice(list(OPERATORS.keys()))
-    correct_answer = str(OPERATORS[operator](first_number, second_number    task = f'Question: {first_number} {operator} {second_number}'
+    correct_answer = str(OPERATORS[operator](first_number, second_numbertask = f'Question: {first_number} {operator} {second_number}'
     return correct_answer, task
