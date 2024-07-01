18b56afd8a0bdabb5535594d1652be62da628346
Michael <168862237+n3wbiexgod@users.noreply.github.com>
2024-07-01 15:34:57 +0400

    Update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,20 +5,16 @@ from math import sqrt
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def make_question():
-    task = randint(2, 99)
-    flag = is_prime(task)
-    if flag is True:
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
-    return correct_answer, task
-
-
-def is_prime(task):
-    i = 2
-    while i <= sqrt(task):
-        if task % i == 0:
+def is_prime(number):
+    if number == 1:
+        return False
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+    i = 3
+    while i <= sqrt(number):
+        if number % i == 0:
             return False
-        i += 1
+        i += 2
     return True
