998e080335b1e5a30ffab0e68c077e188ddea606
Michael <168862237+n3wbiexgod@users.noreply.github.com>
2024-07-01 15:01:21 +0400

    Update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.engine import logic
+from brain_games.engine import run
 from brain_games.games import progression
 
 
 def main():
-    logic(progression)
+    run(progression)
 
 
 if __name__ == '__main__':
