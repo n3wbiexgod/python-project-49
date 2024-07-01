154d006376a5b0d76eb7c7e2e95a4101d9086bd0
Michael <168862237+n3wbiexgod@users.noreply.github.com>
2024-07-01 15:14:58 +0400

    Update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,8 +1,5 @@
 from operator import add, sub, mul
-from random import randint, choice
 
-
-DESCRIPTION = 'What is the result of the expression?'
 OPERATORS = {
     '+': add,
     '-': sub,
@@ -11,13 +8,8 @@ OPERATORS = {
 
 
 def make_question():
-
     first_number, second_number = randint(1, 50), randint(1, 50)
     operator = choice(list(OPERATORS.keys()))
-    correct_answer = str(abs(OPERATORS[operator](first_number, second_number)))
-    if first_number > second_number:
-        task = f'{first_number} {operator} {second_number}'
-    elif second_number > first_number:
-        task = f'{second_number} {operator} {first_number}'
+    correct_answer = str(OPERATORS[operator](first_number, second_number    task = f'{first_number} {operator} {second_number}'
 
     return correct_answer, task
