d90d39ed86e4ac057324b7530966c0f08f979153
Michael <168862237+n3wbiexgod@users.noreply.github.com>
2024-07-01 15:00:03 +0400

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.engine import logic
+from brain_games.engine import run
 from brain_games.games import even
 
 
 def main():
-    logic(even)
+    run(even)
 
 
 if __name__ == '__main__':
