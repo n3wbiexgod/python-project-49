33fbbf596850dc8a30fcbfd3d50048efc267f417
Michael <168862237+n3wbiexgod@users.noreply.github.com>
2024-07-02 05:03:51 +0400

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,22 +4,23 @@ DESCRIPTION = 'What number is missing in the progression?'
 
 
 def make_progression():
-
+    """Генерирует арифметическую прогрессию и определяет пропущенное число."""
     task = []
-    initial_number, difference = randint(0, 50), randint(0, 50)
+    initial_number = randint(0, 50)
+    difference = randint(1, 10)
     length = 10
-    for index in range(length):
-        initial_number += difference
+    for _ in range(length):
         task.append(initial_number)
+        initial_number += difference
 
-    random_index = randint(0, 9)
+    random_index = randint(0, length - 1)
     correct_answer = str(task[random_index])
-    return task, correct_answer, random_index
+    task[random_index] = '..'
+    return task, correct_answer
 
 
 def make_question():
-
-    task, correct_answer, random_index = make_progression()
-    task[random_index] = '..'
-    task = ' '.join(str(i) for i in task)
-    return correct_answer, task
+    """Формирует вопрос с прогрессией, где одно из чисел заменено на '..'"""
+    task, correct_answer = make_progression()
+    task_display = ' '.join(str(i) for i in task)
+    return correct_answer, task_display
